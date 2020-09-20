5d5ec5ce1333b398c30d86be3b9ba7a93a5ef51c
Chris Williams <williaster@users.noreply.github.com>
2020-09-19 18:52:25 -0700

    project: rename vx => visx (#803)
    * rename(demo, part i)
    
    * rename(demo, part ii)
    
    * rename(demo, part iii)
    
    * rename(demo, part iv)
    
    * rename(annotation)
    
    * rename(axis)
    
    * rename(bounds)
    
    * rename(brush)
    
    * rename(chord)
    
    * rename(clip-path)
    
    * rename(curve, i)
    
    * rename(curve, ii)
    
    * rename(drag, i)
    
    * rename(drag, ii)
    
    * rename(event, i)
    
    * rename(event, ii)
    
    * rename(geo, i)
    
    * rename(geo, ii)
    
    * rename(glyph, i)
    
    * rename(glyph, ii)
    
    * rename(gradient, i)
    
    * rename(gradient, ii)
    
    * rename(grid, i)
    
    * rename(grid, ii)
    
    * rename(group, i)
    
    * rename(group, ii)
    
    * rename(heatmap, i)
    
    * rename(heatmap, ii)
    
    * rename(hierarchy, i)
    
    * rename(hierarchy, ii)
    
    * rename(legend, i)
    
    * rename(legend, ii)
    
    * rename(marker, i)
    
    * rename(marker, ii)
    
    * rename(mock-data, i)
    
    * rename(mock-data, ii)
    
    * rename(network, i)
    
    * rename(network, ii)
    
    * rename(pattern, i)
    
    * rename(pattern, ii)
    
    * rename(point, i)
    
    * rename(point, ii)
    
    * rename(react-spring, i)
    
    * rename(react-spring, ii)
    
    * rename(responsive, i)
    
    * rename(responsive, ii)
    
    * rename(scale, i)
    
    * rename(scale, ii)
    
    * rename(shape, i)
    
    * rename(shape, ii)
    
    * rename(stats, i)
    
    * rename(stats, ii)
    
    * rename(text, i)
    
    * rename(text, ii)
    
    * rename(threshold, i)
    
    * rename(threshold, ii)
    
    * rename(tooltip, i)
    
    * rename(tooltip, ii)
    
    * rename(voronoi, i)
    
    * rename(voronoi, ii)
    
    * rename(xychart, i)
    
    * rename(xychart, ii)
    
    * rename(zoom, i)
    
    * rename(zoom, ii)
    
    * rename(umbrella, i)
    
    * rename(umbrella, ii)
    
    * rename(root misc)
    
    * rename(demo, v)
    
    * rename(demo url)
    
    * fix(demo): misc fixes
    
    * rename(xychart): misc fixes
    
    * rename(demo/xychart): misc fixes
    
    * rename: vx.demo.now.sh => airbnb.io/visx
    
    * rename(README, visx-demo.hmtl): misc vx => visx

---
+++ src/scales/symlog.ts
@@ -0,0 +1,20 @@
+import { scaleSymlog } from 'd3-scale';
+import { DefaultOutput } from '../types/Base';
+import { PickScaleConfigWithoutType } from '../types/ScaleConfig';
+import scaleOperator from '../operators/scaleOperator';
+
+export const updateSymlogScale = scaleOperator<'symlog'>(
+  'domain',
+  'range',
+  'reverse',
+  'clamp',
+  'constant',
+  'nice',
+  'zero',
+);
+
+export default function createSymlogScale<Output = DefaultOutput>(
+  config?: PickScaleConfigWithoutType<'symlog', Output>,
+) {
+  return updateSymlogScale(scaleSymlog<Output>(), config);
+}
